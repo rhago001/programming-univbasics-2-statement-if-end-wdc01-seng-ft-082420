4c7f8d1626f9b6339a0a3cd23b4ec543ceda2ad4
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-25 03:48:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -8,14 +8,7 @@ current_time = current_time.to_i
 if current_time.even?
   puts "Even!"
 else
-  puts "odd!"
+  puts "Odd!"
 end 
 
-current_time = Time.now
-current_time = current_time.to_i
- 
-if current_time.even?
-  puts "Even!"
-else
-  puts "Odd!"
-end
+
